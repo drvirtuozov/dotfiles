50a10ab215d015aa82612a84109c9b60236b49e1
drvirtuozov <drvirtuozov@ya.ru>
2017-09-20 14:17:16 +0300

    Add vscode config

--- install.sh
+++ install.sh
@@ -1,11 +1,16 @@
 #!/usr/bin/env bash
 
-# Clementine player config
-echo Installing clementime player config...
-mkdir ~/.config/Clementine
+# Clementine Player config
+echo Installing clementime config...
+mkdir -p ~/.config/Clementine
 ln -sfv $(pwd)/.config/Clementine/Clementine.conf ~/.config/Clementine
 
 # NVIM config
 echo Installing nvim config...
-mkdir ~/.config/nvim
+mkdir -p ~/.config/nvim
 ln -sfv $(pwd)/.config/nvim/init.vim ~/.config/nvim
+
+# Visual Studio Code config
+echo Installing vscode config...
+mkdir -p ~/.config/Code/User
+ln -sfv $(pwd)/.config/Code/User/settings.json ~/.config/Code/User/settings.json
