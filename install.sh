e6930419f7d5b28ca8e8dbe5df4222799850f911
drvirtuozov <drvirtuozov@ya.ru>
2018-06-07 01:32:09 +0300

    Rearrange files

--- install.sh
+++ install.sh
@@ -3,17 +3,17 @@
 # Clementine Player config
 echo Installing clementime config...
 mkdir -p ~/.config/Clementine
-ln -sfv $(pwd)/.config/Clementine/Clementine.conf ~/.config/Clementine
+ln -sfv $(pwd)/Clementine.conf ~/.config/Clementine
 
 # NVIM config
 echo Installing nvim config...
 mkdir -p ~/.config/nvim
-ln -sfv $(pwd)/.config/nvim/init.vim ~/.config/nvim
+ln -sfv $(pwd)/init.vim ~/.config/nvim
 
 # Visual Studio Code config
 echo Installing vscode config...
 mkdir -p ~/.config/Code/User
-ln -sfv $(pwd)/.config/Code/User/settings.json ~/.config/Code/User/settings.json
+ln -sfv $(pwd)/vscode.json ~/.config/Code/User/settings.json
 
 
 # Proxy Automatic Configuration
